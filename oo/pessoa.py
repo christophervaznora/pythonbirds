e702f4bf1a0b7bd3cc7a1eea3a362440f754f45d
Christopher Vaz <christopher_vaz@hotmail.com>
2022-04-19 21:16:49 -0300

    criados atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,18 @@
 class Pessoa:
+    def __int__(self, nome=None, idade=27):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'hello {id(self)}'
+
 
 if __name__ == '__main__':
     p = Pessoa()
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    p.nome = 'Christopher'
+    print(p.nome)
+    print(p.idade)
+
